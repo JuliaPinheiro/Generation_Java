2ec403acf9b7c3ba934fc44826ae738416bb0c56
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-10-29 16:54:57 -0300

    Resolved merge conflicts

---
+++ src/aula07/Ex05.java
@@ -0,0 +1,15 @@
+package aula07;
+
+public class Ex05 {
+public static void main(String[] args) {
+	
+	 System.out.printf("%-20s %-20s %-15s%n", "Código do Produto", "Produto", "Preço Unitário");
+     System.out.println("---------------------------------------------------------------");
+     System.out.printf("%-20d %-20s %-15s%n", 1, "Cachorro Quente", "R$ 10.00");
+     System.out.printf("%-20d %-20s %-15s%n", 2, "X-Salada", "R$ 15.00");
+     System.out.printf("%-20d %-20s %-15s%n", 3, "X-Bacon", "R$ 18.00");
+     System.out.printf("%-20d %-20s %-15s%n", 4, "Bauru", "R$ 12.00");
+     System.out.printf("%-20d %-20s %-15s%n", 5, "Refrigerante", "R$ 8.00");
+     System.out.printf("%-20d %-20s %-15s%n", 6, "Suco de Laranja", "R$ 13.00");
+}
+}
