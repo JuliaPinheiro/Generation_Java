3565b3ac2c033cc17650260eb7ad0429002ae636
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-10-30 17:19:54 -0300

    feat: ex01 laços de repetição

---
+++ src/laco/repeticao/Ex01.java
@@ -0,0 +1,30 @@
+package laco.repeticao;
+
+import java.util.Scanner;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int numberOne, numberTwo;
+
+		System.out.print("Informe o primeiro número ");
+		numberOne = scanner.nextInt();
+
+		System.out.print("Informe o segundo número ");
+		numberTwo = scanner.nextInt();
+
+		if (numberOne >= numberTwo) {
+			System.out.println("intervalo inválido!!");
+		} else {
+			System.out.println("No intervalo entre " + numberOne + " e " + numberTwo + ":");
+			for (int i = numberOne; i <= numberTwo; i++) {
+				if (i % 3 == 0 && i % 5 == 0) {
+
+					System.out.println(i + " é multiplo de 3 e 5");
+				}
+			}
+		}
+
+		scanner.close();
+	}
+}
