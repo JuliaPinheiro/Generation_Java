394bddd4cc80f1f4f473f02687b67d95bcbe65cf
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-10-30 19:35:11 -0300

    feat: ex06 laços de repetição

---
+++ src/laco/repeticao/Ex06.java
@@ -0,0 +1,29 @@
+package laco.repeticao;
+
+import java.util.Scanner;
+
+public class Ex06 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number;
+		int count = 0;
+		float sum = 0;
+
+		do {
+			System.out.print("Digite um número: ");
+			number = scanner.nextInt();
+
+			if (number % 3 == 0 && number != 0) {
+				sum += number;
+				count++;
+			}
+
+		} while (number != 0);
+
+		float average = (count > 0) ? sum / count : 0;
+
+		System.out.println("A média de todos os números múltiplos de 3 é: " + average);
+
+		scanner.close();
+	}
+}
