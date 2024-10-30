ebc64a5d5aa316130fe746786997791e055108db
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-10-30 19:25:12 -0300

    feat: ex05 laços de repetição

---
+++ src/laco/repeticao/Ex05.java
@@ -0,0 +1,25 @@
+package laco.repeticao;
+
+import java.util.Scanner;
+
+public class Ex05 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int number;
+		float sum = 0;
+
+		do {
+			System.out.println("Digite um número: ");
+			number = scanner.nextInt();
+
+			if (number > 0) {
+				sum += number;
+			}
+		} while (number != 0);
+
+		System.out.println("A soma dos números positivos é: " + sum);
+
+	}
+
+}
