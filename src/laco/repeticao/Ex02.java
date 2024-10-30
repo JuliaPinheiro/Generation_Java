a3b5ffe5613070743c872f497baa8068e248f264
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-10-30 17:54:55 -0300

    feat: ex02 laços de repetição

---
+++ src/laco/repeticao/Ex02.java
@@ -0,0 +1,25 @@
+package laco.repeticao;
+
+import java.util.Scanner;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int counterPair = 0, counterOdd = 0;
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("Informe o " + i + " º número inteiros: ");
+			int number = scanner.nextInt();
+
+			if (number % 2 == 0) {
+				counterPair += 1;
+			} else {
+				counterOdd += 1;
+			}
+
+			System.out.println("Total de números pares: " + counterPair + " Total de números ímpares: " + counterOdd);
+		}
+	}
+
+}
