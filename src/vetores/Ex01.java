623136f41ea96ad0ef09acfa481507ee3842076c
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-10-31 10:56:20 -0300

    feat: vetor 01

---
+++ src/vetores/Ex01.java
@@ -0,0 +1,31 @@
+package vetores;
+
+import java.util.Scanner;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		boolean encontrado = false;
+
+		int[] numeros = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+
+		System.out.print("Informe um número entre 1 - 10: ");
+		int opcao = scanner.nextInt();
+
+		for (int i = 0; i < numeros.length; i++) {
+
+			if (opcao == numeros[i]) {
+				System.out.println("o número " + opcao + " esta na posição " + i);
+				encontrado = true;
+			}
+		}
+
+		if (!encontrado) {
+			System.out.println("Não foi encontrado!");
+		}
+		scanner.close();
+	}
+
+}
